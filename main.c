054dcdd77f4aec8b353c709438f2713837c71602
mansuiki <mansuiki@gmail.com>
2019-05-14 22:48:50 +0900

    각 맵마다 배열 남는 부분없이 출력하는 기능 + 설명 주석 추가

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-// 캐릭터 움직이기 기본 기능 + 뼈대 작성 필요
+char temp[5][30][30]; // 처음 맵파일을 읽어와 데이터를 저장하는 변수
+char map[5][30][30]; // temp 변수를 가공하여 저장하는 변수
 
-char temp[5][30][30];
 
-
-void loadmap(void)
+void loadmap(void) // 맵파일에서 데이터를 불러와 temp 에 저장하고 가공하여 map 에 저장하는 함수
 {
     FILE *ifp;
     int i1 = 0, i2 = 0, i3 = 0, c = 0, yes = 0;
@@ -53,7 +52,7 @@ void loadmap(void)
             case '\n':
                 if (yes == 0)
                     yes = 1;
-                if (yes == 1)
+                else if (yes == 1)
                     i2++;
                 i3 = 0;
                 break;
@@ -66,23 +65,11 @@ void loadmap(void)
                 i3++;
                 break;
         }
-
-
     }
+
     loadmapend:
 
     fclose(ifp);
-
-}
-
-
-
-
-int main(void)
-{
-
-    loadmap();
-    char map[5][30][30];
 
     for (int i1 = 0; i1 <= 4; i1++)
     {
@@ -91,17 +78,95 @@ int main(void)
             for (int i3 = 0; i3 <= 29; i3++)
             {
                 if (temp[i1][i2][i3] == NULL)
-                    map[i1][i2][i3] = ' ';
+                    map[i1][i2][i3] = 'C';
                 else
                     map[i1][i2][i3] = temp[i1][i2][i3];
-                printf("%c", map[i1][i2][i3]);
+            }
+        }
+    }
+}
+
+
+int checkXsize(int i1) // 배열의 X 사이즈를 알아내는 함수
+{
+    int size = 0, i2 = 0;
+
+    for (int i3 = 0; i3 <= 29; i3++)
+    {
+        if (map[i1][i2][i3] == 'C')
+        {
+            size = i3;
+            break;
+        }
+    }
+
+    return size;
+}
+
+
+int checkYsize(int i1, int Xsize) // 배열의 Y 사이즈를 알아내는 함수
+{
+    int size = 0;
+    int i3 = Xsize - 1;
+
+    for (int i2 = 0; i2 <= 29; i2++)
+    {
+        if (map[i1][i2][i3] == 'C')
+        {
+            size = i2;
+            break;
+        }
+    }
+
+    return size;
+}
+
+
+int main(void)
+{
+
+    loadmap();
+
+    char map1[checkXsize(0)][checkYsize(0, checkXsize(0))];
+    char map2[checkXsize(1)][checkYsize(1, checkXsize(1))];
+    char map3[checkXsize(2)][checkYsize(2, checkXsize(2))];
+    char map4[checkXsize(3)][checkYsize(3, checkXsize(3))];
+    char map5[checkXsize(4)][checkYsize(4, checkXsize(4))];
+
+    for (int i = 0; i <= 4; i++)
+    {
+        for (int i1 = 0; i1 < checkYsize(i, checkXsize(i)); i1++)
+        {
+            for (int i2 = 0; i2 < checkXsize(i); i2++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        map1[i1][i2] = map[i][i1][i2];
+                        printf("%c", map1[i1][i2]);
+                        break;
+                    case 1:
+                        map2[i1][i2] = map[i][i1][i2];
+                        printf("%c", map2[i1][i2]);
+                        break;
+                    case 2:
+                        map3[i1][i2] = map[i][i1][i2];
+                        printf("%c", map3[i1][i2]);
+                        break;
+                    case 3:
+                        map4[i1][i2] = map[i][i1][i2];
+                        printf("%c", map4[i1][i2]);
+                        break;
+                    case 4:
+                        map5[i1][i2] = map[i][i1][i2];
+                        printf("%c", map5[i1][i2]);
+                        break;
+                }
             }
             printf("\n");
         }
-
         printf("\n\n");
     }
-
 
 
 
